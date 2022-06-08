e58e5d62d69066dcb9f41cc16e874a8c820b3a7d
Hamza Qahoush <hamza.qah@gmail.com>
2022-06-08 23:03:50 +0300

    login , logut finshed

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -125,4 +125,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CRISPY_TEMPLATE_PACK='bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'  # to redirect home after login
+LOGIN_URL = "login" # redirect user if you want access "log in required page"
