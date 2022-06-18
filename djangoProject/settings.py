7301d0d5079ba5a4b9769e73b03a31832f0f43da
Hamza Qahoush <hamza.qah@gmail.com>
2022-06-18 10:07:15 +0300

    deploy commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -112,9 +112,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # full path where uploaded files on system
 MEDIA_URL = '/media/'  # how access the image in browser,
